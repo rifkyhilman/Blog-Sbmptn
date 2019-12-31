5e2aa947f8097cb4bd77933114867e45e7e141d7
rifkyhilman <rifkyhilman265@gmail.com>
2019-12-31 18:40:08 +0700

    firts comit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Home from "./page/Home.svelte";
+</script>
+
+<div class="body">
+  <Home />
+</div>
---
+++ src/page/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Navbar from "./components/navbar.svelte";
+  import Jumbotron from "./components/jumbotron.svelte";
+  import Cards from "./components/cards.svelte";
+</script>
+
+<style>
+  @media (min-width: 640px) {
+    .body {
+      max-width: none;
+    }
+  }
+</style>
+
+<div class="body">
+  <Navbar />
+  <Jumbotron />
+  <Cards />
+</div>
