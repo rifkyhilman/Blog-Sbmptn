6be62c1f2f08e2d4d8464832553b252bed8bb4c0
rifkyhilman <rifkyhilman265@gmail.com>
2020-01-03 23:47:25 +0700

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
   import Navbar from "./components/navbar.svelte";
+  import Footer from "./components/footer.svelte";
   import Home from "./page/Home.svelte";
   import Blog from "./page/Blog.svelte";
   export let url = "";
@@ -8,9 +9,9 @@
 
 <Router {url}>
   <Navbar />
-
   <div class="body">
     <Route path="/blog" component={Blog} />
     <Route path="/" component={Home} />
   </div>
+  <Footer />
 </Router>
---
+++ src/components/footer.svelte
@@ -0,0 +1,5 @@
+<div class="h-50 p-3 bg-secondary text-white">
+  <ul>
+    <ol>hello</ol>
+  </ul>
+</div>
