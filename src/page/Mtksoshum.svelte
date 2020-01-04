f13672cf1ffaa5aaa9565af1185f6db978c2b8ec
rifkyhilman <rifkyhilman265@gmail.com>
2020-01-04 23:07:44 +0700

    add content mtksoshum

--- src/page/Mtksoshum.svelte
+++ src/page/Mtksoshum.svelte
@@ -2,18 +2,59 @@
   .content {
     margin-top: 10%;
   }
+
+  h1 {
+    border-bottom: 2px solid blue;
+  }
 </style>
 
-<div class="content bg-transparent text-dark">
+<div class="content">
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
-        <a href="#">Home</a>
+        <a href="/">Home</a>
       </li>
       <li class="breadcrumb-item">
-        <a href="#">Library</a>
+        <a href="/">UTBK</a>
       </li>
-      <li class="breadcrumb-item active" aria-current="page">Data</li>
+      <li class="breadcrumb-item active" aria-current="page">MTK_Soshum</li>
     </ol>
   </nav>
+</div>
+<div class="d-flex justify-content-center m-5">
+  <h1>Matematika Soshum UTBK</h1>
+</div>
+<div class="container">
+  <div class="m-5">
+    <p>
+      Di laman ini, kamu akan belajar materi matematika untuk UTBK Matematika
+      Dasar yang lengkap banget. Mulai dari prinsip dasar matematika, persamaan
+      garis, persamaan kuadrat, eksponen dan logaritma, fungsi kuadrat,
+      pertidaksamaan, barisan deret dan bilangan, trigonometri, matriks, program
+      linear, statistika, limit, turunan, fungsi, integral suku banyak,
+      lingkaran, vektor, dimensi tiga, dan masih banyak lagi.
+    </p>
+  </div>
+  <div class="m-5">
+    <div class="accordion" id="accordionExample">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h2 class="mb-0">
+            <a href="/">
+              <h6>Postulat dasar</h6>
+            </a>
+          </h2>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h2 class="mb-0">
+            <a href="/">
+              <h6>Exponen dan logoritma</h6>
+            </a>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
