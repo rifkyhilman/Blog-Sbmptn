3c55b064ab09555e0eab1088988d6467684d3d57
rifkyhillman26 <rifkyhilman265@gmail.com>
2020-01-07 21:17:06 +0700

    add footer 2

--- src/components/cards.svelte
+++ src/components/cards.svelte
@@ -3,55 +3,59 @@
   href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
   integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
   crossorigin="anonymous" />
-
-<div class="card-deck">
-  <div class="card d-flex justify-content-center align-items-center">
-    <img
-      src="http://pngimg.com/uploads/dollar_sign/dollar_sign_PNG21539.png"
-      class="card-img-top w-50 h-60"
-      alt="..." />
-    <div class="card-body">
-      <h5 class="card-title d-flex justify-content-center">Premium</h5>
-      <p class="card-text">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
+<div>
+  <div class="d-flex justify-content-center m-5">
+    <h1>kenapa memilih Ribook ?</h1>
   </div>
-  <div class="card d-flex justify-content-center align-items-center">
-    <img
-      src="http://pngimg.com/uploads/dollar_sign/dollar_sign_PNG28.png"
-      class="card-img-top w-50 h-60"
-      alt="..." />
-    <div class="card-body">
-      <h5 class="card-title d-flex justify-content-center">High</h5>
-      <p class="card-text">
-        This card has supporting text below as a natural lead-in to additional
-        content.
-      </p>
+  <div class="card-deck">
+    <div class="card d-flex justify-content-center align-items-center">
+      <img
+        src="http://pngimg.com/uploads/dollar_sign/dollar_sign_PNG21539.png"
+        class="card-img-top w-50 h-60"
+        alt="..." />
+      <div class="card-body">
+        <h5 class="card-title d-flex justify-content-center">Mudah</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+    <div class="card d-flex justify-content-center align-items-center">
+      <img
+        src="http://pngimg.com/uploads/dollar_sign/dollar_sign_PNG28.png"
+        class="card-img-top w-50 h-60"
+        alt="..." />
+      <div class="card-body">
+        <h5 class="card-title d-flex justify-content-center">High</h5>
+        <p class="card-text">
+          This card has supporting text below as a natural lead-in to additional
+          content.
+        </p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
     </div>
-  </div>
-  <div class="card d-flex justify-content-center align-items-center">
-    <img
-      src="https://cdn1.iconfinder.com/data/icons/shopping-and-ecommerce-2/64/E-commerce_and_Shopping-80-512.png"
-      class="card-img-top w-50 h-60"
-      alt="..." />
-    <div class="card-body">
-      <h5 class="card-title d-flex justify-content-center">VIP</h5>
-      <p class="card-text">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+    <div class="card d-flex justify-content-center align-items-center">
+      <img
+        src="https://cdn1.iconfinder.com/data/icons/shopping-and-ecommerce-2/64/E-commerce_and_Shopping-80-512.png"
+        class="card-img-top w-50 h-60"
+        alt="..." />
+      <div class="card-body">
+        <h5 class="card-title d-flex justify-content-center">VIP</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This card has even longer content than the
+          first to show that equal height action.
+        </p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
     </div>
   </div>
 </div>
--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,33 +2,53 @@
   h1 {
     font-family: "Ma Shan Zheng", cursive;
     font-family: "Girassol", cursive;
-    font-size: 60px;
+    font-size: 100px;
     color: whitesmoke;
   }
 
   a {
     color: whitesmoke;
   }
+
+  .nav1 {
+    margin-right: 60px;
+  }
 </style>
 
-<div
-  class="h-50 p-3 bg-secondary text-white d-flex justify-content-center
-  align-items-center flex-column">
-  <div>
-    <a class="navbar-brand" href="/">
-      <h1>Ribook</h1>
-    </a>
+<div class="h-50 p-3 bg-secondary text-white">
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="logo">
+      <a class="navbar-brand" href="/">
+        <h1>Ribook</h1>
+      </a>
+    </div>
+    <div class="nav1">
+      <h2>About</h2>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2>Contact</h2>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
   </div>
-  <ul>
-    <ol>
-      <a href="/">
-        <img src="./images/email.png" alt="email" />
-      </a>
-    </ol>
-    <ol>
-      <a href="/">
-        <img src="./images/whatsapp.png" alt="email" />
-      </a>
-    </ol>
-  </ul>
 </div>
